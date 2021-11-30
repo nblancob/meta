f13b080c629bb0cef971e57310e024b7637c20ce
nblancob <nblancob@unal.edu.co>
2021-11-29 21:44:58 -0500

    add students queries

--- graphql-server/src/resolvers.js
+++ graphql-server/src/resolvers.js
@@ -1,5 +1,6 @@
-'use strict'
+"use strict";
 const connectDB = require("./database");
+const { ObjectId } = require("mongodb");
 module.exports = {
   Query: {
     getProjects: async () => {
@@ -7,11 +8,95 @@ module.exports = {
       let Projects = [];
       try {
         db = await connectDB();
-        Projects = await db.collection('Projects').find().toArray();
+        Projects = await db.collection("Projects").find().toArray();
       } catch (error) {
         console.log(error);
       }
       return Projects;
+    },
+    getActiveProjects: async () => {
+      let db;
+      let Projectsact = [];
+      try {
+        db = await connectDB();
+        Projectsact = await db
+          .collection("Projects")
+          .find({ state: true })
+          .toArray();
+      } catch (error) {
+        console.log(error);
+      }
+      return Projectsact;
+    },
+    getProjectByID: async (root, { id }) => {
+      let db;
+      let Project;
+      try {
+        db = await connectDB();
+        Project = await db
+          .collection("Projects")
+          .findOne({ _id: ObjectId(id) });
+      } catch (error) {
+        console.log(error);
+      }
+      return Project;
+    },
+    getAdvances: async () => {
+      let db;
+      let Advances = [];
+      try {
+        db = await connectDB();
+        Advances = await db.collection("Advances").find().toArray();
+      } catch (error) {
+        console.log(error);
+      }
+      return Advances;
+    },
+  },
+  Mutation: {
+    createAdvance: async (root, { input }) => {
+      const newAdvance = Object.assign(input);
+      let db;
+      let Advances;
+      try {
+        db = await connectDB();
+        Advances = db.collection("Advances").insertOne(newAdvance);
+      } catch (error) {
+        console.log(error);
+      }
+      return newAdvance;
+    },
+    addAdvance: async (root, { id, input }) => {
+      let db;
+      let Project;
+      try {
+        db = await connectDB();
+        await db
+          .collection("Projects")
+          .updateOne({ _id: ObjectId(id) }, { $push: input });
+        Project = await db
+          .collection("Projects")
+          .findOne({ _id: ObjectId(id) });
+      } catch (error) {
+        console.log(error);
+      }
+      return Project;
+    },
+    editAdvance: async (root, { id, input }) => {
+      let db;
+      let Advance;
+      try {
+        db = await connectDB();
+        await db
+          .collection("Advances")
+          .updateOne({ _id: ObjectId(id) }, { $set: input });
+        Advance = await db
+          .collection("Advances")
+          .findOne({ _id: ObjectId(id) });
+      } catch (error) {
+        console.log(error);
+      }
+      return Advance;
     },
   },
 };
